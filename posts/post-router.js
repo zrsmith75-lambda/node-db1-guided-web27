bfa54efbfe17cb2f8e40f43fbda7185d1cd21e2d
Zac Smith <zrs3141592@gmail.com>
2020-03-16 11:58:13 -0600

    create GET, POST, GET /:id

--- posts/post-router.js
+++ posts/post-router.js
@@ -1,23 +1,52 @@
 const express = require('express');
 
 // database access using knex
-const db = require('../data/db-config.js');
+// connection to the db
+const db = require('../data/db-config.js'); 
 
 const router = express.Router();
   
 router.get('/', (req, res) => {
-  res.status(200).json({ message: 'none'})
+  db.select('*').from('posts')
+  .then(rows => {
+    res.status(200).json(rows)
+  })
+  .catch(err => {
+    res.status(500).json({ message: `${err}`})
+  })
+  
 });
 
 router.get('/:id', (req, res) => {
-
+  db('posts')
+  .where({ id: req.params.id })   // .where("id", "=", req.params.id)
+  .first()
+  .then(post => {
+    if (post) {
+      res.status(200).json({ data: post })
+    } else {
+      res.status(404).json({ message: '404 Post not found'})
+    }
+  })
+  .catch(err => {
+    res.status(500).json({
+      message: `${err}`
+    })
+  })
 });
 
 router.post('/', (req, res) => {
-
+  db('posts').insert(req.body, "id")  // 2nd argument never leave this out, normalizes out differences in sql db
+  .then(ids => {
+    res.status(201).json({ results: ids })
+  })
+  .catch(err => {
+    res.status(500).json({ message: `${err}`})
+  })
 });
 
 router.put('/:id', (req, res) => {
+  
 
 });
 
